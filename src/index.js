288b494e2c44c6bbb8ed60a6b987b01f2476be5a
Marco <marco.com17@gmail.com>
2021-05-25 00:23:40 -0500

    Juego terminado

--- src/index.js
+++ src/index.js
@@ -2,32 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
-  render() {
-    return (
-      <button 
-	className="square" 
-	onClick={() => { this.setState({value: 'X'}) }}>
-        { this.state.value }
-      </button>
-    );
-  }
+/*
+  * Este es un function component, lo podemos
+  * identificar porque no tiene estado y solo
+  * tiene la opcion return
+  * */
+function Square(props) {
+  /*
+    * Este componente es un componente controlado
+    * ya que el componente padre tiene todo el
+    * control sobre este
+    * */
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}>
+      { props.value }
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    /*
+      * Elebamos los estados al componente padre para tener
+      * control desde aqui, ya que tratar de pedirle el estado a
+      * los componentes hijo es mucho mas complejo y dificil de 
+      * reparar en caso de error
+      * */
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+	value={ this.state.squares[i] } 
+	onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -74,3 +113,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+//Para calcular el ganador
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
